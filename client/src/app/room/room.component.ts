cd0676d1547d1f48dc19b796f006697c876c8516
Hlynur Stefánsson <hlynurs15@ru.is>
2017-02-18 18:06:10 +0000

    set up chat window for room

--- client/src/app/room/room.component.ts
+++ client/src/app/room/room.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ViewChild, ElementRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -6,7 +6,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 	templateUrl: './room.component.html',
 	styleUrls: ['./room.component.css']
 })
-export class RoomComponent implements OnInit {
+export class RoomComponent implements OnInit, AfterViewChecked {
+	@ViewChild('scrollMe') private myScrollContainer: ElementRef;
 
 	roomId: string;
 
@@ -15,5 +16,16 @@ export class RoomComponent implements OnInit {
 
 	ngOnInit() {
 		this.roomId = this.route.snapshot.params['id'];
+		this.scrollToBottom();
+	}
+
+	ngAfterViewChecked() {
+		this.scrollToBottom();
+	}
+
+	scrollToBottom(): void {
+		try {
+			this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+		} catch(err) { }
 	}
 }
